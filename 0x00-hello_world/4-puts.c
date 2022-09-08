a4343e774480ae7576b57ae49d949dfed6314cbb
bollo-omar <bolloaggrey@gmail.com>
2022-09-08 13:43:30 -0700

    Task 04 output txt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+/**
+ *main : program entry point
+ *
+ *return : main method terminates with 0 forn success
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
