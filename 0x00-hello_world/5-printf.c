f339e60c266fcd5d26f9a20f7f29097eabbe952e
bollo-omar <bolloaggrey@gmail.com>
2022-09-08 14:02:02 -0700

    second rvison task 05

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- * main - entry point for the program
+ * main - Entry point
  *
- * Return : terminates with 0 if success
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
