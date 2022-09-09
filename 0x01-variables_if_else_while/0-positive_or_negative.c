3c714d8fa39a9d084707606bef4b14bc4d7e27c0
bollo-omar <bolloaggrey@gmail.com>
2022-09-09 12:56:17 -0700

    Task 0 revision

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,16 +15,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf(n + " is positive\n");
 	}
 	else
 		if (n < 0)
 		{
-			printf("is negative\n");
+			printf(n + " is negative\n");
 		}
 		else
 		{
-			printf("is zero\n");
+			printf(n + " is zero\n");
 		}
 	return (0);
 }
