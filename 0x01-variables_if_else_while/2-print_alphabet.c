98fea5549afd4a66960909230a148d1cc532a800
bollo-omar <bolloaggrey@gmail.com>
2022-09-09 13:32:58 -0700

    task 03

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdiguui::o.h>
+ioipiopimopimpomp
+
+
+/**
+ * main - entry point.
+ *
+ * Return: Stop pragram by returnin 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
