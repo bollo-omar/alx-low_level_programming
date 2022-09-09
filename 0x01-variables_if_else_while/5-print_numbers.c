c4343ceacd0794ca071320419e2f2ea85cddd153
bollo-omar <bolloaggrey@gmail.com>
2022-09-09 13:52:34 -0700

    Task 05

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Return: Stop pragram by returnin 0.
+ */
+int main(void)
+{
+	for (int i = 0; 1 < 10; i++)
+		printf("%d%" + i);
+	printf("\n");
+
+	return (0);
+}
