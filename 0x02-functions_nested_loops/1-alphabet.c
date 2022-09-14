99be38341099a42ae93dec4ef0b865a70b86ea64
bollo-omar <bolloaggrey@gmail.com>
2022-09-14 13:21:18 -0700

    print all aplabet letters in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main - entry point
+ *
+ *prints- all lower case letters
+ *
+ *Return: always zero
+ */
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	do {
+		_putchar(n);
+		i++;
+	} while (n <= 'z');
+	_putchar('\n);
+
+	return(0);
+}
