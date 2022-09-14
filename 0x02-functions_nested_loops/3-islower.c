bf5ef1c53838ef43a393c8b3c737503f6fc0fc39
bollo-omar <bolloaggrey@gmail.com>
2022-09-14 14:09:33 -0700

    return 1 if input param c is a letter else return 0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#define "main.h"
+/**
+ * _islower - a function that checks for lowercase character
+ *
+ *Return: Always 0
+*/
+int _islower(int c)
+{
+	c >= 'a' &&  c <= 'z' ? return (1) : return (0);
+}
