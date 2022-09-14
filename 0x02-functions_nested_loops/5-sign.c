7cfae959ad5e0209bad68509353232b986ce12ba
bollo-omar <bolloaggrey@gmail.com>
2022-09-14 14:56:07 -0700

    print + and return 1 if n>0 and 0 and return 0 n==0 or print - and return -1 if n<0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,11 +7,11 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		_putchar("+");
+		_putchar('+');
 		return (1);
 	if (n == 0)
-		_putchar("0");
+		_putchar('0');
 		return (0);
-	_putchar("-");
+	_putchar('-');
 	return (-1);
 }
