be41292e441b760fc07a7f02c985651d12485353
bollo-omar <bolloaggrey@gmail.com>
2022-09-15 03:44:45 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,35 +6,41 @@
 */
 void print_times_table(int n)
 {
-	if (n >= 0 && n <= 15)
+	int x, y, z;
+
+	if (n >= 0 && n <= 14)
 	{
-		int row;
-		int col;
-		int d;
-
-		for (row = 0; row <= n; row++)
+		for (x = 0; x <= n; x++)
 		{
-			_putchar('0');
-			_putchar(',');
-			_putchar(' ');
-
-			for (col = 1; col <= n; col++)
+			for (y = 0; y <= n; y++)
 			{
-				d = row * col;
-
-				if ((d / (n + 1)) > 0)
+				z = x * y;
+				if (z > 99)
 				{
-					_putchar((d / (n + 1)) + '0');
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
 				else
 				{
-					_putchar(' ');
-				}
-				_putchar((d % (n + 1)) + '0');
-				if (col < n)
-				{
-					_putchar(',');
-					_putchar(' ');
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
 				}
 			}
 			_putchar('\n');
