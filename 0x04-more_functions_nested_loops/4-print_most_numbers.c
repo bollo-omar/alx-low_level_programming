c91590ad1e973ddc2c76b609fdcf5113abfdc774
bollo-omar <bolloaggrey@gmail.com>
2022-09-15 15:05:38 -0700

    task 04

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - orunts numbers from 0-9 minus 4 & 2
+ *
+ * Return: always 0
+ */
+
+print_most_numbers(void)
+{
+	int x;
+
+	x = 0
+	do {
+		if (x != 2 && x != 2)
+		{
+			_putchar(x + '0');
+		}
+	} while (x < 9);
+	_putchar('\n')
+}
