f1d5728a07adc9961c8096edda1d3a4590673f90
bollo-omar <bolloaggrey@gmail.com>
2022-09-19 06:01:15 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function reset_to_98
+ *
+ * @n: takes param n as a pointer
+ *
+ * Return: no return type(void method)
+*/
+
+void reset_to_98(int *n)
+{
+	int num = 98;
+	*n = &num;
+}
