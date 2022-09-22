2f0b69c3b88e2cac35eb2bbcc701ec5e2d405a2c
bollo-omar <bolloaggrey@gmail.com>
2022-09-22 15:27:40 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatinates two strings
+ *
+ *  @dest: param destination
+ *  @src: param source
+ *  @n: param n
+ *
+ *  Return:  pointer to dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, largosrc;
+
+	for (i = 0; dest[a] != '\0'; i++)
+	{
+	}
+	for (largosrc = 0; src[largosrc] != 0; largosrc++)
+	{
+	}
+	for (j = 0; j < n && j < largosrc; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	return (dest);
+}
