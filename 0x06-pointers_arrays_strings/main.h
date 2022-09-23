9a2234c788539dd416465e59add9ed80ce571ba3
bollo-omar <bolloaggrey@gmail.com>
2022-09-22 19:03:08 -0700

    Task 12

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif /*MAIN_H*/
