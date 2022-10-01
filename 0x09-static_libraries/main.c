6f371891513eeae3e96f08bd6000d90580b3342b
bollo-omar <bolloaggrey@gmail.com>
2022-09-30 17:58:42 -0700

    Task 0 revison

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - main method
+ *
+ * Return: always 0(succes)
+*/
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
