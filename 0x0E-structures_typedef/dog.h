03ff417ecf6b2d00ec7db10ff1b96034721c1097
bollo-omar <bolloaggrey@gmail.com>
2022-10-11 13:44:49 -0700

    Task 00

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog variables
+ * @name: name
+ * @age: age of dog
+ * @owner: name of owner
+*/
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
